026b305f4979652fffaee04e1df7163cd1f899e8
billygun27 <billygun27@gmail.com>
2019-03-05 20:37:22 +0700

    distillation h2py

--- yolo_distillation2.py
+++ yolo_distillation2.py
@@ -4,15 +4,14 @@ Retrain the YOLO model for your own dataset.
 
 import numpy as np
 import keras.backend as K
-import h5py
-from keras.layers import Input, Lambda
+from keras.layers import Input, Lambda ,Reshape
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 
 from model.core import preprocess_true_boxes, yolo_loss
-from model.mobilenet import yolo_body
-from model.yolo3 import  tiny_yolo_body
+from model.mobilenet import mobilenetv2_yolo_body
+from model.yolo3 import yolo_body, tiny_yolo_body
 from model.utils  import get_random_data
 
 import argparse
@@ -29,7 +28,7 @@ def _main():
     anchors = get_anchors(anchors_path)
 
     input_shape = (416,416) # multiple of 32, hw
-    
+
     is_tiny_version = len(anchors)==6 # default setting
     if is_tiny_version:
         model = create_tiny_model(input_shape, anchors, num_classes,
@@ -43,22 +42,55 @@ def _main():
         monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
-    
-  #  with open(train_path) as f:
-  #      train_lines = f.readlines()
 
-  #  with open(val_path) as f:
-  #      val_lines = f.readlines()
+    with open(train_path) as f:
+        train_lines = f.readlines()
+
+    with open(val_path) as f:
+        val_lines = f.readlines()
 
    # with open(test_path) as f:
    #     test_lines = f.readlines()
 
-    train_lines =  h5py.File('train_logits.h5','r') #np.load('train_logits.npy')[()]
-    val_lines = h5py.File('val_logits.h5','r') #np.load('val_logits.npy')[()]
+   # train_lines = np.load('train_logits.npy')[()]
+   # val_lines = np.load('val_logits.npy')[()]
+
     num_val = int(len(train_lines))
     num_train = int(len(val_lines))
 
-   
+    num_anchors = len(anchors)
+    image_input = Input(shape=(416, 416, 3))
+    teacher = yolo_body(image_input, num_anchors//3, num_classes)
+    teacher.load_weights("model_data/trained_weights_final.h5")
+    
+    # return the constructed network architecture
+    # class+5
+    yolo3 = Reshape((13, 13, 3, 25))(teacher.layers[-3].output)
+    yolo2 = Reshape((26, 26, 3, 25))(teacher.layers[-2].output)
+    yolo1 = Reshape((52, 52, 3, 25))(teacher.layers[-1].output)
+    
+    teacher = Model( input= teacher.input , output=[yolo3,yolo2,yolo1] )
+    '''
+    batch_size = 2
+    i = 0 #step
+    for  logits in data_generator_wrapper(train_lines, batch_size, input_shape, anchors, num_classes,teacher) : 
+        #x , y = dat
+        #train_logits[i] = logits
+        #trainY[i] = dat
+        #print(x.shape)
+        print(logits[0][0].shape)
+        print(logits[0][1].shape)
+        #print(logits[0][1])
+        #print( logits[1] )
+        #print( train_logits[0][0][1].shape)
+        #print( len( train_logits[0][1] ) )
+        #print(i)
+        #print(img.shape)
+        #print(dat)
+        i+=1
+        if i>= 3:#(len(train_lines)//batch_size+1) :
+            break
+    '''
     # Train with frozen layers first, to get a stable loss.
     # Adjust num epochs to your dataset. This step is enough to obtain a not bad model.
     if True:
@@ -66,16 +98,16 @@ def _main():
             # use custom yolo_loss Lambda layer.
             'yolo_loss': lambda y_true, y_pred: y_pred})
 
-        batch_size = 2#16#32
+        batch_size = 1#16#32
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit_generator(data_generator_wrapper(train_lines, batch_size, input_shape, anchors, num_classes),
+        model.fit_generator(data_generator_wrapper(train_lines, batch_size, input_shape, anchors, num_classes,teacher),
                 steps_per_epoch=max(1, num_train//batch_size),
-                validation_data=data_generator_wrapper(val_lines, batch_size, input_shape, anchors, num_classes),
+                validation_data=data_generator_wrapper(val_lines, batch_size, input_shape, anchors, num_classes,teacher),
                 validation_steps=max(1, num_val//batch_size),
                 epochs=50,
                 initial_epoch=0,
                 callbacks=[logging, checkpoint])
-        model.save_weights(log_dir + 'trained_distillation_weights_stage_1_mobilenet_v2.h5')
+        model.save_weights(log_dir + 'trained_weights_stage_1.h5')
 
     # Unfreeze and continue training, to fine-tune.
     # Train longer if the result is not good.
@@ -85,16 +117,16 @@ def _main():
         model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         print('Unfreeze all of the layers.')
 
-        batch_size =  2#16#32 note that more GPU memory is required after unfreezing the body
+        batch_size =  1#16#32 note that more GPU memory is required after unfreezing the body
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit_generator(data_generator_wrapper(train_lines, batch_size, input_shape, anchors, num_classes),
+        model.fit_generator(data_generator_wrapper(train_lines, batch_size, input_shape, anchors, num_classes,teacher),
             steps_per_epoch=max(1, num_train//batch_size),
-            validation_data=data_generator_wrapper(val_lines, batch_size, input_shape, anchors, num_classes),
+            validation_data=data_generator_wrapper(val_lines, batch_size, input_shape, anchors, num_classes,teacher),
             validation_steps=max(1, num_val//batch_size),
             epochs=100,
             initial_epoch=50,
             callbacks=[logging, checkpoint, reduce_lr, early_stopping])
-        model.save_weights(log_dir + 'trained_distillation_weights_final_mobilenet_v2.h5')
+        model.save_weights(log_dir + 'trained_weights_final.h5')
 
     # Further training if needed.
 
@@ -125,7 +157,7 @@ def create_model(input_shape, anchors, num_classes, load_pretrained=True, freeze
     y_true = [Input(shape=(h//{0:32, 1:16, 2:8}[l], w//{0:32, 1:16, 2:8}[l], \
         num_anchors//3, num_classes+5)) for l in range(3)]
 
-    model_body = yolo_body(image_input, num_anchors//3, num_classes)
+    model_body = mobilenetv2_yolo_body(image_input, num_anchors//3, num_classes)
     print('Create YOLOv3 model with {} anchors and {} classes.'.format(num_anchors, num_classes))
 
     if load_pretrained:
@@ -174,42 +206,33 @@ def create_tiny_model(input_shape, anchors, num_classes, load_pretrained=True, f
 
     return model
 
-def data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes):
+def data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes,teacher):
     '''data generator for fit_generator'''
-
-
     n = len(annotation_lines)
     i = 0
     while True:
         image_data = []
-        bbox_data = []
-        mbox_data = []
-        sbox_data = []
+        box_data = []
         for b in range(batch_size):
-            #if i==0:
-                #np.random.shuffle(annotation_lines)
-           # print(i)
-           # print(annotation_lines[i][0].shape)
-           # print(annotation_lines[i][1].shape)
-            image, bbox , mbox ,sbox =  annotation_lines["img_data"][i] , annotation_lines["big_logits"][i] , annotation_lines["medium_logits"][i] , annotation_lines["small_logits"][i]  #get_random_data(annotation_lines[i], input_shape, random=True)
+            if i==0:
+                np.random.shuffle(annotation_lines)
+            image, box = get_random_data(annotation_lines[i], input_shape, random=True)
             image_data.append(image)
-            bbox_data.append(bbox)
-            mbox_data.append(mbox)
-            sbox_data.append(sbox)
+            box_data.append(box)
             i = (i+1) % n
         image_data = np.array(image_data)
-        bbox_data = np.array(bbox_data)
-        mbox_data = np.array(mbox_data)
-        sbox_data = np.array(sbox_data)
+        #box_data = np.array(box_data)
         #y_true = preprocess_true_boxes(box_data, input_shape, anchors, num_classes)
-        #y_true = model.predict(image_data)
-      
-        yield [image_data, bbox_data,  mbox_data , sbox_data], np.zeros(batch_size)
+        box = teacher.predict(image_data)
+        #np.savez('temp_logits.npz', 0 = box[0] , 1=box[1] , 2=box[2] )
+        #box = np.load('temp_logits.npz')[()]
+        y_true = box
+        yield [image_data, *y_true], np.zeros(batch_size)
 
-def data_generator_wrapper(annotation_lines, batch_size, input_shape, anchors, num_classes):
+def data_generator_wrapper(annotation_lines, batch_size, input_shape, anchors, num_classes,teacher):
     n = len(annotation_lines)
     if n==0 or batch_size<=0: return None
-    return data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes)
+    return data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes,teacher)
 
 if __name__ == '__main__':
     _main()
--- yolo_logits.py
+++ yolo_logits.py
@@ -7,6 +7,7 @@ import matplotlib.image as mpimg
 
 import numpy as np
 import keras.backend as K
+import h5py
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Input, Lambda ,Reshape
 from keras.models import Model
@@ -17,6 +18,8 @@ from model.core import preprocess_true_boxes, yolo_loss
 from model.yolo3 import yolo_body, tiny_yolo_body
 from model.utils import get_random_data
 
+from tqdm import tqdm
+import time
 
 def _main():
     train_path = '2007_train.txt'
@@ -40,63 +43,143 @@ def _main():
 
    # with open(test_path) as f:
    #     test_lines = f.readlines()
-    '''
+
+    num_anchors = len(anchors)
     image_input = Input(shape=(416, 416, 3))
     model = yolo_body(image_input, num_anchors//3, num_classes)
     model.load_weights("model_data/trained_weights_final.h5")
-
     
-    # class+5
     yolo3 = Reshape((13, 13, 3, 25))(model.layers[-3].output)
     yolo2 = Reshape((26, 26, 3, 25))(model.layers[-2].output)
     yolo1 = Reshape((52, 52, 3, 25))(model.layers[-1].output)
     
 
-    model = Model( input= model.input , output=[yolo3,yolo2,yolo1] )
-    #model.summary()
-
-    model.compile(
-        optimizer=Adam(lr=1e-3) , 
-        loss='categorical_crossentropy', metrics=['accuracy']
-    )
-    '''
+    model = Model( inputs= model.input , outputs=[yolo3,yolo2,yolo1] )
     
     batch_size = 1
     
-    train_logits = {}
-    val_logits = {}
+    print( "total "+ str(len(train_lines)) + " loop "+ str( len(train_lines) ) )
 
-    logits_box = []
-    print( "total "+ str(len(train_lines)) + " loop "+ str( len(train_lines)//batch_size +1 ) )
+        # create an hdf5 file
+    train_size = len(train_lines)
+    with h5py.File("train_logits.h5",'w') as f:
+        # create a dataset for your movie
+        img = f.create_dataset("img_data", shape=(  train_size, 416, 416, 3)) #len(train_lines)
+        bbox = f.create_dataset("big_logits", shape=( train_size, 13, 13, 3, 25))
+        mbox = f.create_dataset("medium_logits", shape=( train_size , 26, 26, 3, 25))
+        sbox = f.create_dataset("small_logits", shape=(  train_size , 52, 52, 3, 25))
+
+        # fill the 10 frames with a random image
+        i = 0
+        for logits in tqdm( data_generator_wrapper(train_lines, batch_size, input_shape, anchors, num_classes,model) ) : 
+            #print(logits[0][0])
+            #print(logits[0][0].shape)
+            #trat = logits[0][0]
+
+            #print("box")
+            #print(logits[1][0])
+            #print(logits[1][0].shape)
+
+            img[i] = logits[0][0] # np.random.randint(255, size=(416, 416, 3)) #        
+            bbox[i] = logits[1][0]
+            mbox[i] = logits[2][0]
+            sbox[i] = logits[3][0]
+        
+            i+=1
+            if i>= train_size:#(len(train_lines)) :
+                break
+
+
+    #fp = h5py.File('train_logits.h5','r')
+    #train_logits = []
+    #print(fp["img_data"][0].shape)
+    #print(fp["big_logits"][1].shape)
+    #print(fp["medium_logits"][1].shape)
+
+    #print("o1")
+    #print(fp["img_data"][0])
+    #print("o2")
+    #print(fp["img_data"][1])
+    #print("ddd")
+    #print(trat)
+
+    #print("b1")
+    #print(fp["big_logits"][2])
+    #print("b2")
+    #print(fp["big_logits"][1])
+    #print("ddd")
+    #print(trat)
+
+
+    #train_logits.append(fp["img_data"][0])
+    #train_logits.append(fp["img_data"][1])
+
+    #train_logits = np.array(train_logits)
+    #print(train_logits.shape)
+
+    val_size = len(val_lines)
+    with h5py.File("val_logits.h5",'w') as f:
+        # create a dataset for your movie
+        img = f.create_dataset("img_data", shape=(  val_size, 416, 416, 3)) #
+        bbox = f.create_dataset("big_logits", shape=( val_size, 13, 13, 3, 25))
+        mbox = f.create_dataset("medium_logits", shape=( val_size, 26, 26, 3, 25))
+        sbox = f.create_dataset("small_logits", shape=(  val_size, 52, 52, 3, 25))
+
+        # fill the 10 frames with a random image
+        i = 0
+        for logits in tqdm( data_generator_wrapper(val_lines, batch_size, input_shape, anchors, num_classes,model) ) : 
+
+            img[i] = logits[0][0] # np.random.randint(255, size=(416, 416, 3)) #        
+            bbox[i] = logits[1][0]
+            mbox[i] = logits[2][0]
+            sbox[i] = logits[3][0]
+        
+            i+=1
+            if i>= val_size:#(len(val_lines)) :
+                break
+
+'''
     i = 0 #step
     for  logits in data_generator_wrapper(train_lines, batch_size, input_shape, anchors, num_classes) : 
         #x , y = dat
-        train_logits[i] = logits
-        
+        #train_logits[i] = logits
+        print(logits[0].shape)
+        print(logits[1].shape)
+        f = h5py.File('train_logits.h5','a')
+        f.create_dataset('data_logits', data=logits)
+        f.close()    
+
+
         i+=1
-        if i>= (len(train_lines)) :
+        if i>= 5:#(len(train_lines)) :
             break
     
+    fp = h5py.File('train_logits.h5','r')
+    print(fp[0][1].shape)
+    print(fp[1][2].shape)
 
-   # print(train_logits[0][1].shape)
+    # print(train_logits[0][1].shape)
+    #np.save('train_logits.npy', train_logits)
+    #train_log = np.load('train_logits.npy')[()]
 
+    #print(train_log[0][0].shape)
+    #print(train_log[1][0].shape)
+    
     print( "total "+ str(len(val_lines)) + " loop "+ str( len(val_lines)//batch_size +1 ) )
     i = 0 #step
     for  logits in data_generator_wrapper(val_lines, batch_size, input_shape, anchors, num_classes) : 
         #x , y = dat
         val_logits[i] = logits
+
         i+=1
         if i>= (len(val_lines)) :
             break
     
 
-    np.save('train_logits.npy', train_logits)
-    np.save('val_logits.npy', val_logits)
+    '''
+    #np.save('val_logits.npy', val_logits)
 
-    train_log = np.load('train_logits.npy')[()]
-
-    print(train_log[0][0].shape)
-    print(train_log[1][0].shape)
+    
 
 
 def get_classes(classes_path):
@@ -114,20 +197,8 @@ def get_anchors(anchors_path):
     return np.array(anchors).reshape(-1, 2)
 
 
-def data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes):
+def data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes,model):
     '''data generator for fit_generator'''
-
-    num_anchors = len(anchors)
-    image_input = Input(shape=(416, 416, 3))
-    model = yolo_body(image_input, num_anchors//3, num_classes)
-    model.load_weights("model_data/trained_weights_final.h5")
-    
-    yolo3 = Reshape((13, 13, 3, 25))(model.layers[-3].output)
-    yolo2 = Reshape((26, 26, 3, 25))(model.layers[-2].output)
-    yolo1 = Reshape((52, 52, 3, 25))(model.layers[-1].output)
-    
-
-    model = Model( input= model.input , output=[yolo3,yolo2,yolo1] )
 
     n = len(annotation_lines)
     i = 0
@@ -156,10 +227,10 @@ def data_generator(annotation_lines, batch_size, input_shape, anchors, num_class
 
         yield [image_data, *y_true]#, np.zeros(batch_size)
 
-def data_generator_wrapper(annotation_lines, batch_size, input_shape, anchors, num_classes):
+def data_generator_wrapper(annotation_lines, batch_size, input_shape, anchors, num_classes,model):
     n = len(annotation_lines)
     if n==0 or batch_size<=0: return None
-    return data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes)
+    return data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes,model)
 
 if __name__ == '__main__':
     _main()
